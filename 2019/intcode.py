29e91602c7f4eb637ddf2003f27c03900c45b441
Roberto Liffredo <rliffredo@outlook.com>
2019-12-13 00:37:10 +0100

    Small performance improvement on intcode

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,20 +1,21 @@
 class Instruction:
-    MODE_INDIRECT = '0'
-    MODE_DIRECT = '1'
-    MODE_RELATIVE = '2'
+    MODE_INDIRECT = 0
+    MODE_DIRECT = 1
+    MODE_RELATIVE = 2
 
-    def __init__(self, computer, instruction_info, amt_parameters):
+    stop = False
+    size = 1  # opcode at a very minimum
+
+    __slots__ = ['instruction_info', 'computer', 'mem', 'next_ip']
+
+    def __init__(self, computer, instruction_info):
         self.instruction_info = instruction_info
-        self.size = amt_parameters + 1  # add opcode
         self.computer = computer
         self.mem = computer.memory
-        self.ip = computer.ip
-        self.r1 = computer.r1
         self.next_ip = None
-        self.stop = False
 
     def advance_ip(self):
-        return self.ip + self.size if self.next_ip is None else self.next_ip
+        return self.computer.ip + self.size if self.next_ip is None else self.next_ip
 
     def fetch(self, i):
         param_address = self.data_address(i)
@@ -29,65 +30,82 @@ class Instruction:
 
     def data_address(self, i):
         mode = self.instruction_info[i - 1]
-        if mode == Instruction.MODE_INDIRECT:
-            address = self.mem[self.ip + i]
-        elif mode == Instruction.MODE_DIRECT:
-            address = self.ip + i
-        elif mode == Instruction.MODE_RELATIVE:
-            address = self.r1 + self.mem[self.ip + i]
-        else:
-            assert False, 'Invalid mode'
-        return address
+        if mode is Instruction.MODE_DIRECT:
+            return self.computer.ip + i
+        elif mode is Instruction.MODE_RELATIVE:
+            return self.computer.r1 + self.mem[self.computer.ip + i]
+        else:  # mode is Instruction.MODE_INDIRECT:
+            return self.mem[self.computer.ip + i]
 
     def __repr__(self):
-        return f'[{type(self).__name__.ljust(8)}] ip: {self.ip}, r1: {self.r1}, size: {self.size}, ' \
-               f'mem: {[self.mem[self.ip + m] for m in range(self.size)]}'
+        return f'[{type(self).__name__.ljust(8)}] ip: {self.computer.ip}, r1: {self.computer.r1}, size: {self.size}, ' \
+               f'mem: {[self.mem[self.computer.ip + m] for m in range(self.size)]}'
+
+
+class Start(Instruction):
+    def __init__(self, computer):
+        super().__init__(computer, (0, 0, 0))
+        self.next_ip = 0
+
+    def apply(self):
+        pass
 
 
 class Stop(Instruction):
+    stop = True
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 0)
-        self.stop = True
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         pass
 
 
 class Add(Instruction):
+    size = 4
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 3)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         self.store(3, self.fetch(1) + self.fetch(2))
 
 
 class Mul(Instruction):
+    size = 4
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 3)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         self.store(3, self.fetch(1) * self.fetch(2))
 
 
 class Input(Instruction):
+    size = 2
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 1)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         self.store(1, self.computer.input_provider())
 
 
 class Output(Instruction):
+    size = 2
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 1)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         self.computer.output_provider(self.fetch(1))
 
 
 class JumpTrue(Instruction):
+    size = 3
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 2)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         if self.fetch(1):
@@ -95,8 +113,10 @@ class JumpTrue(Instruction):
 
 
 class JumpFalse(Instruction):
+    size = 3
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 2)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         if not self.fetch(1):
@@ -104,24 +124,30 @@ class JumpFalse(Instruction):
 
 
 class LessThan(Instruction):
+    size = 4
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 3)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         self.store(3, 1 if self.fetch(1) < self.fetch(2) else 0)
 
 
 class Equals(Instruction):
+    size = 4
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 3)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         self.store(3, 1 if self.fetch(1) == self.fetch(2) else 0)
 
 
 class AdjustR1(Instruction):
+    size = 2
+
     def __init__(self, computer, instruction_info):
-        super().__init__(computer, instruction_info, 1)
+        super().__init__(computer, instruction_info)
 
     def apply(self):
         self.computer.r1 += self.fetch(1)
@@ -129,27 +155,29 @@ class AdjustR1(Instruction):
 
 class IntCode:
     INSTRUCTIONS = {
-        '01': Add,
-        '02': Mul,
-        '03': Input,
-        '04': Output,
-        '05': JumpTrue,
-        '06': JumpFalse,
-        '07': LessThan,
-        '08': Equals,
-        '09': AdjustR1,
-        '99': Stop,
+        1: Add,
+        2: Mul,
+        3: Input,
+        4: Output,
+        5: JumpTrue,
+        6: JumpFalse,
+        7: LessThan,
+        8: Equals,
+        9: AdjustR1,
+        99: Stop,
     }
 
     MAX_PARAMETERS = 4
+    instructions_cache = {}
 
     def fetch(self, code_data):
-        full_opcode = str(code_data).rjust(IntCode.MAX_PARAMETERS + 2, '0')
-        if self.debug:
-            print(full_opcode)
-        opcode = full_opcode[-2:]
-        param_modes = list(reversed(full_opcode[:-2]))
-        return IntCode.INSTRUCTIONS[opcode], param_modes
+        if code_data not in self.instructions_cache:
+            opcode = code_data % 100
+            p1 = (code_data // 100) % 10
+            p2 = (code_data // 1000) % 10
+            p3 = (code_data // 10000) % 10
+            self.instructions_cache[code_data] = IntCode.INSTRUCTIONS[opcode], (p1, p2, p3)
+        return self.instructions_cache[code_data]
 
     def __init__(self, input_provider=None, output_provider=None, debug=False):
         self.debug = debug
@@ -184,18 +212,13 @@ class IntCode:
         self.memory[2] = verb
 
     def execute(self):
-        self.ip = 0
-        while True:
-            # print(self)
+        instruction = Start(self)
+        while not instruction.stop:
+            self.ip = instruction.advance_ip()
             instruction_info = self.memory[self.ip]
             opcode, param_modes = self.fetch(instruction_info)
             instruction = opcode(self, param_modes)
-            if self.debug:
-                print(instruction)
-            if instruction.stop:
-                break
             instruction.apply()
-            self.ip = instruction.advance_ip()
 
     def get_output(self):
         return self.memory[0]
