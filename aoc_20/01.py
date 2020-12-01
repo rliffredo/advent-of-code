0029281f85ffb95c46b892110889fa000031190a
Roberto Liffredo <rliffredo@outlook.com>
2020-12-01 11:14:11 +0100

    Added new directory for aoc20, and also updated to latest PyCharm. Added Day 1 solution.

---
+++ aoc_20/01.py
@@ -0,0 +1,36 @@
+raw_numbers = open('data/01.txt').readlines()
+all_numbers = {int(n) for n in raw_numbers}
+
+
+def find_pairs_for_sum(numbers, k):
+    for number in numbers:
+        other_number = k - number
+        if other_number in numbers:
+            return True, number, other_number
+    else:
+        return False, 0, 0
+
+
+res = find_pairs_for_sum(all_numbers, 2020)
+if res[0]:
+    print(f"Found {res[1]} and {res[2]}, their product is {res[1]*res[2]}")
+else:
+    print("No pair found!")
+
+
+def find_thrice_for_sum(numbers, k):
+    numbers_to_check = {n for n in numbers}
+    while numbers_to_check:
+        n0 = numbers_to_check.pop()
+        partial_sum = k-n0
+        res, n1, n2 = find_pairs_for_sum(numbers_to_check, partial_sum)
+        if res:
+            return res, n0, n1, n2
+    return False, 0, 0, 0
+
+
+res = find_thrice_for_sum(all_numbers, 2020)
+if res[0]:
+    print(f"Found {res[1]}, {res[2]}, {res[3]}, their product is {res[1]*res[2]*res[3]}")
+else:
+    print("No thrice found!")
---
+++ aoc_20/README.md
@@ -0,0 +1,9 @@
+## Day 1: Report Repair
+
+I actually forgot about AoC, this year, so I started it late.
+
+So, today's task was quite a typical interview coding task, with just some story
+about Christmas (small OT: I really hope we are going to save Santa again!).  
+This was one I was fully prepared, having done it countless times; but I suspect
+that the second part, without an efficient first part, could have taken a bit
+too much time.
