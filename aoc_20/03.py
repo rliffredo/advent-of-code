1b082f20b464e4b0d063eedc12e76bdc1da7cae0
Roberto Liffredo <rliffredo@outlook.com>
2020-12-03 08:52:54 +0100

    Day 3 solution

---
+++ aoc_20/03.py
@@ -0,0 +1,43 @@
+from math import prod
+from typing import Tuple, Iterator
+
+from common import read_data
+
+
+def parse_data():
+    raw_map = read_data("03", True)
+    tree_map = {(x, y) for y, line in enumerate(raw_map) for x, char in enumerate(line) if char == "#"}
+    return tree_map, len(raw_map[0]), len(raw_map)
+
+
+def make_slope(slope_params: Tuple[int, int], map_size: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
+    x, y = 0, 0
+    while True:
+        if y > map_size[1]:
+            return
+        yield x, y
+        x = (x + slope_params[0]) % map_size[0]
+        y = y + slope_params[1]
+
+
+def get_hits_in_slope(map_info, x, y):
+    tree_map, size_x, size_y = map_info
+    slope = make_slope((x, y), (size_x, size_y))
+    return sum(1 for position in slope if position in tree_map)
+
+
+def part_1():
+    map_info = parse_data()
+    hit_tree = get_hits_in_slope(map_info, 3, 1)
+    print(f"Trees hit: {hit_tree}")
+
+
+def part_2():
+    map_info = parse_data()
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    hit_tree = prod(get_hits_in_slope(map_info, *slope) for slope in slopes)
+    print(f"Trees hit: {hit_tree}")
+
+
+part_1()
+part_2()
--- aoc_20/README.md
+++ aoc_20/README.md
@@ -17,3 +17,18 @@ This time I am using less ipython, and more python with type hinting. I am not
 sure if this is not removing the fun of the thing, or it's just that I got used
 to it. But I also appreciate taking it a bit slower, and going in a simpler
 and steadier way.
+
+## Day 3: Toboggan Trajectory
+
+And we start with maps :-)
+
+With the experience of previous years, it was not a difficult task. The final
+solution is smaller than the previous day, even if it required a bit more
+programming.  
+However, it took me a bit too much time than I was expecting. Eventually, I had
+to debug to discover a silly off-by-one mistake. I suspect the decision to wake
+up earlier to work on the problem had an impact on my performance :-)  
+
+I like having decided to use a generator for representing the function. It made
+the solution much nicer to see, and the second part almost a breeze.  
+Also, I have learnt a nice new corner of python 3.8: `math.prod`.
